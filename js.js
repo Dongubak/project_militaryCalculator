e7cf2c9bed5c03d0588757e6d0a412252a48862e
Dongubak <91736442+Dongubak@users.noreply.github.com>
2021-12-24 23:34:57 +0900

    복무일 계산기
    복무일 계산기 js

--- js.js
+++ js.js
@@ -5,17 +5,56 @@ function init() {
 }
 
 function function1(event) {
-    event.preventDefault();
-
-    const startDate = new Date(document.querySelector("#startDate").value);
-    const endDate = new Date(document.querySelector("#endDate").value);
-    const result = endDate.getTime() - startDate.getTime();
-    const MILLTODAY = 1000*60*60*24;
-
-    console.log(result/MILLTODAY);
-
     
+    event.preventDefault();
+    clear();
+    const [start, end, today] = [...inputValue()];
+    const [all, rest, percent] = uls();
+    
+    const allDay = Math.ceil(dateCal(end, start));
+    const restDay = Math.ceil(dateCal(end, today));
+    const percentDay = Math.ceil((allDay - restDay) / allDay * 100);
+    console.log(allDay, restDay, percentDay);
+    addlist(all, allDay);
+    addlist(rest, restDay);
+    addlist(percent, percentDay);
 
 }
+
+function dateCal() {
+    const arr = [...arguments];
+    const timeCal = arr[0].getTime() - arr[1].getTime();
+    const MILLITODAY = 1000 * 60 * 60 * 24;
+    return timeCal / MILLITODAY;
+}
+
+function uls() {
+    return [...document.getElementsByTagName('ul')];
+}
+
+function inputValue() {
+    const startDate = new Date(document.querySelector("#startDate").value);
+    const endDate = new Date(document.querySelector("#endDate").value);
+    const today = new Date();  
+    
+    return [startDate, endDate, today];
+}
+
+function addlist(location, value) {
+    const textNode = document.createTextNode(value);
+    const listNode = document.createElement('li');
+    listNode.appendChild(textNode);
+    location.appendChild(listNode);
+}
+
+function clear() {
+    const ulChild = uls();
+
+    for (const items of ulChild) {
+        items.removeChild(items.children[1]);
+    }
+    
+}
+///date객체 전달
 
 init();
