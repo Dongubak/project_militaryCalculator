5c350e2f7f970cadf125003c48f2b531bdb52706
Dongubak <91736442+Dongubak@users.noreply.github.com>
2021-12-25 22:59:45 +0900

    update
    update

--- js.js
+++ js.js
@@ -1,24 +1,19 @@
 function init() {
     const btn = document.querySelector("#btn");
-
     btn.addEventListener("click", function1);
 }
 
 function function1(event) {
-    
     event.preventDefault();
     clear();
     const [start, end, today] = [...inputValue()];
     const [all, rest, percent] = uls();
-    
     const allDay = Math.ceil(dateCal(end, start));
     const restDay = Math.ceil(dateCal(end, today));
     const percentDay = Math.ceil((allDay - restDay) / allDay * 100);
-    console.log(allDay, restDay, percentDay);
     addlist(all, allDay);
     addlist(rest, restDay);
     addlist(percent, percentDay);
-
 }
 
 function dateCal() {
@@ -36,7 +31,6 @@ function inputValue() {
     const startDate = new Date(document.querySelector("#startDate").value);
     const endDate = new Date(document.querySelector("#endDate").value);
     const today = new Date();  
-    
     return [startDate, endDate, today];
 }
 
@@ -49,11 +43,9 @@ function addlist(location, value) {
 
 function clear() {
     const ulChild = uls();
-    console.log(ulChild);
     for (const items of ulChild) {
         items.removeChild(items.children[1]);
     }
 }
-///date객체 전달
 
 init();
